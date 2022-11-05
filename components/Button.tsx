56dddd21331905bb1cb9330d1c307a0c900c78da
Yagth <yabuman46@gmail.com>
2022-11-05 19:20:07 +0300

    feat: Created and Added button componenet

---
+++ components/Button.tsx
@@ -0,0 +1,17 @@
+
+interface IProps {
+    buttonType: "button" | "submit" | "reset",
+    text: string,
+    buttonClass: string,
+}
+
+function Button({buttonType, text, buttonClass}:IProps){
+    return (
+        <button type={buttonType}
+                className={"rounded-lg bg-orange-700 text-white transition duration-150 hover:bg-orange-600 w-20 " + buttonClass}>
+        {text}
+        </button>
+    )
+}
+
+export default Button
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,12 +1,12 @@
 import Input from "./Input"
 import Logo from "./Logo"
-import Image from "next/image"
+import Button from "./Button"
 
 
 function Login(){
     return (
         
-        <div className="grid bg-gray-100 place-items-center hover:scale-110 transition duration-150 transform ease-in-out hover:shadow-lg rounded-md h-3/5 p-5">
+        <div className="flex flex-col items-center justify-center bg-gray-100 hover:scale-110 transition duration-150 transform ease-in-out hover:shadow-lg rounded-lg h-3/5 w-1/5 p-5">
 
             <Logo 
                 src="/hot-news-logo.png"
@@ -15,12 +15,21 @@ function Login(){
             <Input 
                 inputName="email"
                 inputType="email"
+                inputClass="mt-6"
             />
 
             <Input
                 inputName="password"
                 inputType="password"
+                inputClass=""
             />
+
+            <Button 
+                buttonType="submit"
+                text="Login"
+                buttonClass="h-9 mt-3"                
+            />
+
       </div>
     )
 
