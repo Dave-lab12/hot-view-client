06026b2b506dd8850f94887399e30852f88e90b1
Yagth <yabuman46@gmail.com>
2022-11-09 16:32:20 +0300

    fix:  Fixed the aweful look of sign up

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -11,42 +11,47 @@ function SignUp(){
         />
         <main>
           <div className="grid place-items-center h-screen">
-            <div className="grid grid-cols-2 items-center justify-center bg-gray-100 hover:scale-110 transition duration-150 transform ease-in-out hover:shadow-lg rounded-lg h-3/5 w-1/4 p-5">
+
+            <div className="grid items-center justify-center bg-gray-100 hover:scale-110 transition duration-150 transform ease-in-out hover:shadow-lg rounded-lg h-3/5 w-1/4 p-5">
 
                 <Logo 
                     src="/hot-news-logo.png"
                 />
 
-                <Input 
-                    inputName="email"
-                    inputType="email"
-                    inputClass="mt-6"
-                />
+                <div className="grid grid-cols-2">
+                    
+                    <Input 
+                        inputName="email"
+                        inputType="email"
+                        inputClass="mt-6"
+                    />
 
-                <Input
-                    inputName="First name"
-                    inputType="text"
-                />
+                    <Input
+                        inputName="First name"
+                        inputType="text"
+                    />
 
-                <Input
-                    inputName="Last name"
-                    inputType="text"
-                />
+                    <Input
+                        inputName="Last name"
+                        inputType="text"
+                    />
 
-                <Input
-                    inputName="password"
-                    inputType="password"
-                />
+                    <Input
+                        inputName="password"
+                        inputType="password"
+                    />
 
-                <Input
-                    inputName="confirm password"
-                    inputType="password"
-                />
+                    <Input
+                        inputName="confirm password"
+                        inputType="password"
+                    />
 
-                <Input 
-                    inputName="phonenumber"
-                    inputType="number"
-                />
+                    <Input 
+                        inputName="phonenumber"
+                        inputType="number"
+                    />
+                    
+                </div>
 
                 <Button
                     buttonType="submit"
