262cee192d7f39d18bc020d1c5fb6d441ae2778f
Yagth <yabuman46@gmail.com>
2022-11-09 12:34:48 +0300

    feat: Added a header component

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+function Header(){
+    return(
+        <Head>
+          <title>SignUp</title>
+          <link 
+            rel="icon" 
+            href="/hot-news-logo.png"
+            type="image"
+          />
+        </Head>
+    )
+
+}
+
+export default Header
---
+++ pages/singUp.tsx
@@ -0,0 +1,20 @@
+import SignUp from "@/components/SignUp";
+import Head from "next/head";
+
+function signIn(){
+    <div className="bg-gray-300 overflow-hidden">
+        <Head>
+          <title>SignUp</title>
+          <link 
+            rel="icon" 
+            href="/hot-news-logo.png"
+            type="image"
+          />
+        </Head>
+        <main>
+          <div className="grid place-items-center h-screen">
+            <SignUp />
+          </div>
+        </main>
+      </div>
+}
