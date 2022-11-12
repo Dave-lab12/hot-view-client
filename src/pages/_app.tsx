8d42d0b75af9e861b1a6e14273493454051dcc6c
dave <dwtmekonnen123@gmail.com>
2022-11-12 18:12:35 +0300

    refactor: updated folder structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "@/styles/global.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
