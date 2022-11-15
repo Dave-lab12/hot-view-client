bccfdfbc755fc04929295838d7d7d3ecad4b290e
Yagth <yabuman46@gmail.com>
2022-11-15 20:26:58 +0300

    refactor: Responsive signup form

--- components/SignUp.tsx
+++ components/SignUp.tsx
@@ -30,111 +30,112 @@ function SignUp() {
     <div className="bg-gray-300 overflow-hidden">
       <Header title="signup" />
       <main>
-        <div className="grid place-items-center h-screen">
-          <div className="grid place-items-center bg-gray-100 hover:shadow-lg rounded-lg h-3/5 w-1/4 px-5">
-            <form onSubmit={formik.handleSubmit} className="grid grid-cols-2">
-              <Logo
-                src="/hot-news-logo.png"
-                logoClass="col-span-2 place-self-center mb-6"
-              />
+        <div className="grid place-items-center h-screen overflow-auto">
+          <div className="grid place-items-center bg-gray-100 hover:shadow-lg max-sm:w-3/4 sm:w-3/4 md:w-1/2 rounded-lg">
+            <Logo
+              src="/hot-news-logo.png"
+              logoClass="place-self-center mb-6 mt-3"
+            />
+            <div className="grid grid-flow-col place-content-evenly-center w-3/4 py-3">
+              <form onSubmit={formik.handleSubmit}>
+                <h1 className="place-self-start text-sm ml-1 mb-2 text-orange-700 ">
+                  Fill the following form please
+                </h1>
 
-              <h1 className="col-span-2 place-self-start text-sm ml-1 mb-2 text-orange-700 ">
-                Fill the following form please
-              </h1>
-
-              <div>
-                <Input
-                  inputName="email"
-                  inputType="email"
-                  inputClass="mr-2 mb-2"
-                  inputId="email"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.email}
-                />
-                {formik.touched.email && formik.errors.email && (
-                  <ErrorSpan errorMessage={formik.errors.email} />
-                )}
-              </div>
-
-              <div>
-                <Input
-                  inputName="phonenumber"
-                  inputType="number"
-                  inputId="phonenumber"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.phonenumber}
-                />
-                {formik.errors.phonenumber && formik.touched.phonenumber && (
-                  <ErrorSpan errorMessage={formik.errors.phonenumber} />
-                )}
-              </div>
-              <div>
-                <Input
-                  inputName="firstname"
-                  inputType="text"
-                  inputId="firstname"
-                  inputClass="mr-2 mb-2"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.firstname}
-                />
-                {formik.touched.firstname && formik.errors.firstname && (
-                  <ErrorSpan errorMessage={formik.errors.firstname} />
-                )}
-              </div>
-
-              <div>
-                <Input
-                  inputName="lastname"
-                  inputType="text"
-                  inputId="lastname"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.lastname}
-                />
-                {formik.errors.lastname && (
-                  <ErrorSpan errorMessage={formik.errors.lastname} />
-                )}
-              </div>
-
-              <div>
-                <Input
-                  inputName="password"
-                  inputType="password"
-                  inputId="pass"
-                  inputClass="mr-2 mb-2"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.password}
-                />
-                {formik.touched.password && formik.errors.password && (
-                  <ErrorSpan errorMessage={formik.errors.password} />
-                )}
-              </div>
-
-              <div>
-                <Input
-                  inputName="confirmPassword"
-                  inputType="password"
-                  inputId="pass2"
-                  changed={formik.handleChange}
-                  blur={formik.handleBlur}
-                  acceptedValue={formik.values.confirmPassword}
-                />
-                {formik.touched.confirmPassword &&
-                  formik.errors.confirmPassword && (
-                    <ErrorSpan errorMessage={formik.errors.confirmPassword} />
+                <div>
+                  <Input
+                    inputName="email"
+                    inputType="email"
+                    inputClass="mr-2 mb-2"
+                    inputId="email"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.email}
+                  />
+                  {formik.touched.email && formik.errors.email && (
+                    <ErrorSpan errorMessage={formik.errors.email} />
                   )}
-              </div>
+                </div>
 
-              <Button
-                buttonType="submit"
-                text="sign up"
-                buttonClass="col-span-2 place-self-center"
-              />
-            </form>
+                <div>
+                  <Input
+                    inputName="phonenumber"
+                    inputType="number"
+                    inputId="phonenumber"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.phonenumber}
+                  />
+                  {formik.errors.phonenumber && formik.touched.phonenumber && (
+                    <ErrorSpan errorMessage={formik.errors.phonenumber} />
+                  )}
+                </div>
+                <div>
+                  <Input
+                    inputName="firstname"
+                    inputType="text"
+                    inputId="firstname"
+                    inputClass="mr-2 mb-2"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.firstname}
+                  />
+                  {formik.touched.firstname && formik.errors.firstname && (
+                    <ErrorSpan errorMessage={formik.errors.firstname} />
+                  )}
+                </div>
+
+                <div>
+                  <Input
+                    inputName="lastname"
+                    inputType="text"
+                    inputId="lastname"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.lastname}
+                  />
+                  {formik.errors.lastname && (
+                    <ErrorSpan errorMessage={formik.errors.lastname} />
+                  )}
+                </div>
+
+                <div>
+                  <Input
+                    inputName="password"
+                    inputType="password"
+                    inputId="pass"
+                    inputClass="mr-2 mb-2"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.password}
+                  />
+                  {formik.touched.password && formik.errors.password && (
+                    <ErrorSpan errorMessage={formik.errors.password} />
+                  )}
+                </div>
+
+                <div>
+                  <Input
+                    inputName="confirmPassword"
+                    inputType="password"
+                    inputId="pass2"
+                    changed={formik.handleChange}
+                    blur={formik.handleBlur}
+                    acceptedValue={formik.values.confirmPassword}
+                  />
+                  {formik.touched.confirmPassword &&
+                    formik.errors.confirmPassword && (
+                      <ErrorSpan errorMessage={formik.errors.confirmPassword} />
+                    )}
+                </div>
+
+                <Button
+                  buttonType="submit"
+                  text="sign up"
+                  buttonClass="place-self-center"
+                />
+              </form>
+            </div>
           </div>
         </div>
       </main>
