3a0d83f445f00c6e13229edb1261a1acdc001d0a
Yagth <yabuman46@gmail.com>
2022-11-20 11:24:04 +0300

    feat: FeaturedNews componenet

---
+++ src/components/HomePage/FeaturedNews.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface IProps {
+  imageUrl: string;
+  newsCategory: string;
+  postedDate: string;
+  postedBy: string;
+  title: string;
+  description: string;
+}
+
+export function FeaturedNews({
+  imageUrl,
+  newsCategory,
+  postedDate,
+  postedBy,
+  title,
+  description,
+}: IProps) {
+  return (
+    <div>
+      <div
+        className="bg-cover bg-no-repeat "
+        style={`background-image: url('${{ imageUrl }}')`}
+      >
+        <div className=" bg-gray-800 bg-opacity-25 ">
+          <div className="flex flex-row place-content-start">
+            <h3>{newsCategory}</h3>
+            <h3>{postedBy}</h3>
+            <h3>{postedDate}</h3>
+          </div>
+          <div>
+            <h1>{title}</h1>
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import FeaturedNews from "../components/HomePage/FeaturedNews";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <FeaturedNews
+        imageUrl="https://media.istockphoto.com/id/1182477852/photo/breaking-news-world-news-with-map-backgorund.jpg?s=612x612&w=0&k=20&c=SQfmzF39HZJ_AqFGosVGKT9iGOdtS7ddhfj0EUl0Tkc="
+        newsCategory="Health"
+        postedDate="November 11 2024"
+        postedBy="News Direct"
+        title="The highest breaking news ever"
+        description="This is just a test news discription"
+      />
+    </>
+  );
 }
