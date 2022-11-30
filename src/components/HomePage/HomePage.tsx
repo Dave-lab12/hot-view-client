bfbf5bf24c5dad3978987bee57a0bf1ae01308f5
Yagth <yabuman46@gmail.com>
2022-11-30 15:48:10 +0300

    feat: Working on NavBar

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,8 +1,36 @@
-import React, { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import * as NavBar from "@radix-ui/react-navigation-menu";
+import { Fragment } from "react";
 
 function HomePage() {
-  return <>HomePage</>;
+  return (
+    <>
+      <div>
+        <p className="space-x-3">
+          Follow us: <i>Facebook</i> <i>Twitter</i> <i>Instagram</i>
+          <i>YouTube</i>
+        </p>
+      </div>
+      <NavBar.Root className="grid grid-flow-col place-content-start p-5 mx-4 border-solid border-b items-center ">
+        <NavBar.List>
+          <NavBar.Link href="/" className="text-2xl text-slate-800 mr-3 ">
+            News Reporting
+          </NavBar.Link>
+        </NavBar.List>
+        <NavBar.List className="mx-4 text-gray-700 text-lg">
+          <NavBar.Link href="/signup">Stories</NavBar.Link>
+        </NavBar.List>
+        <NavBar.List className="mx-4 text-gray-700 text-lg">
+          <NavBar.Link href="/signup">Forum</NavBar.Link>
+        </NavBar.List>
+        <NavBar.List className="mx-4 text-gray-700 text-lg">
+          <NavBar.Link href="/signup">Store</NavBar.Link>
+        </NavBar.List>
+        <NavBar.List className="mx-4 text-gray-700 text-lg">
+          <NavBar.Link href="/signup">Contact</NavBar.Link>
+        </NavBar.List>
+      </NavBar.Root>
+    </>
+  );
 }
 
 export default HomePage;
