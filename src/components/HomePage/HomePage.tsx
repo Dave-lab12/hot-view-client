ee630c03f9451b780880e3cb27bcf176e9a0be17
Yagth <yabuman46@gmail.com>
2022-11-30 14:38:13 +0300

    refactor: Removed radix dependency

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Link from "../Link";
+
+function HomePage() {
+  return <>Homepage</>;
+}
+
+export default HomePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,8 @@
-import SideNews from "@/components/HomePage/News/SideNews";
-import Header from "@/components/Header";
+import React from "react";
+import HomePage from "@/components/HomePage/HomePage";
 
-import FeaturedNews from "../components/HomePage/News/FeaturedNews";
-import TopNewsCard from "../components/HomePage/News/TopNewsCard";
-
-export default function Home() {
-  return (
-    <>
-      <div className="h-screen">
-        <Header title="homepage" />
-        <main>
-          <TopNewsCard
-            imageUrl="/iStockPic.jpg"
-            title="Hello there"
-            postedBy="SomeOne"
-            isVerified={true}
-            views={100}
-            postedDate="2 Days"
-          />
-          <div className="">
-            <FeaturedNews
-              imageUrl="/iStockPic.jpg"
-              newsCategory="Health"
-              postedDate="Nov 11, 2024"
-              postedBy="News Direct"
-              title="The highest breaking news ever"
-              description="This is just a test news discription"
-            />
-            <SideNews
-              imageUrl="/iStockPic.jpg"
-              postedDate="Nov 11, 2024"
-              readingTime={3}
-              title="Health issue that needs immediate attention"
-              imageWidth={140}
-              imageHeight={140}
-            />
-          </div>
-        </main>
-      </div>
-    </>
-  );
+function homepage() {
+  return <HomePage />;
 }
+
+export default homepage;
