60e6e39d20a724c596bef9396e38511271053706
Yagth <yabuman46@gmail.com>
2022-12-01 13:53:40 +0300

    refactor: Changed the gradient overlay

--- src/components/HomePage/News/FeaturedNews.tsx
+++ src/components/HomePage/News/FeaturedNews.tsx
@@ -24,18 +24,20 @@ function FeaturedNews({
   return (
     <>
       <div
-        className=" flex flex-col bg-cover h-full place-content-end text-white"
+        className="flex flex-col bg-cover h-full place-content-end text-white"
         style={{ backgroundImage: `url(${imageUrl})` }}
       >
-        <div className="bg-black bg-opacity-70 ">
-          <div className="flex flex-row text-gray-400 ">
-            <NewsInfo displayInfo={newsCategory} infoClass="px-4" />
-            <NewsInfo displayInfo={postedBy} infoClass="px-4" />
-            <NewsInfo displayInfo={postedDate} infoClass="px-4" />
-          </div>
-          <div className=" flex flex-col px-3 py-4">
-            <NewsHeading title={title} />
-            <NewsDescription description={description} />
+        <div className="flex flex-col place-content-end h-3/4 bg-gradient-to-t from-black to-transparent">
+          <div className=" ">
+            <div className="flex flex-row text-gray-400 ">
+              <NewsInfo displayInfo={newsCategory} infoClass="px-4" />
+              <NewsInfo displayInfo={postedBy} infoClass="px-4" />
+              <NewsInfo displayInfo={postedDate} infoClass="px-4" />
+            </div>
+            <div className=" flex flex-col px-3 py-4">
+              <NewsHeading title={title} />
+              <NewsDescription description={description} />
+            </div>
           </div>
         </div>
       </div>
