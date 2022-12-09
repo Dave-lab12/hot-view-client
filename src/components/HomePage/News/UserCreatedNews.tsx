764a62d4ab2b3b36acc7673f9e1bf5221735d022
Yagth <yabuman46@gmail.com>
2022-12-09 13:11:50 +0300

    refactor: Look of the usercreated card

--- src/components/HomePage/News/UserCreatedNews.tsx
+++ src/components/HomePage/News/UserCreatedNews.tsx
@@ -25,7 +25,7 @@ function UserCreatedNews({
 }: IProps) {
   return (
     <div
-      className={`grid sm:grid-cols- md:grid-cols-3 place-items-start border-b pb-2 ${customClass}`}
+      className={`grid sm:grid-cols-2 md:grid-cols-3 place-items-start border-b pb-2 ${customClass}`}
     >
       <div className="col-span-2  justify-center">
         <div className="flex flex-row items-center">
@@ -52,12 +52,14 @@ function UserCreatedNews({
             titleClass="text-orange-800"
           />
           <NewsDescription
-            descClass="break-normal pr-20"
+            descClass="sm:break-normal sm:pr-20"
             description="User created news description in short which is very highly toxic that is used to do some stuff that are really great"
           />
-          <div className="flex flex-col sm:flex-row items-start md:space-x-16">
-            <NewsInfo infoClass="" displayInfo="Nov 20 2022" />
-            <NewsInfo infoClass="grow" displayInfo="10 min read" />
+          <div className="flex flex-row items-start md:space-x-16">
+            <div className="flex flex-col grow sm:flex-row">
+              <NewsInfo infoClass="" displayInfo="Nov 20 2022" />
+              <NewsInfo infoClass="grow" displayInfo="10 min read" />
+            </div>
             <div className="text-sm md:space-x-5 md:pr-4 text-orange-700">
               <BsBookmark className="inline-block text-xl" />
               <BsThreeDots className="inline-block text-xl" />
@@ -65,7 +67,7 @@ function UserCreatedNews({
           </div>
         </div>
       </div>
-      <div className="col-span-1 palce-self-center md:place-self-end">
+      <div className="col-span-3 sm:col-span-1 place-self-auto md:place-self-end">
         <Image
           src={newsImage}
           className="mt-5 md:mt-0"
