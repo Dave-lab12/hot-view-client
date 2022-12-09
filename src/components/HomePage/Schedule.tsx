be5789ffea0e86a56bdb7f57d7e1fbe736ac65f0
Yagth <yabuman46@gmail.com>
2022-12-09 13:36:13 +0300

    refactor: Schedule calendar Icon

--- src/components/HomePage/Schedule.tsx
+++ src/components/HomePage/Schedule.tsx
@@ -11,17 +11,21 @@ interface IProps {
 
 function Schedule({ title, date, dateTimeStamp, customClass = " " }: IProps) {
   return (
-    <div className={`flex flex-row items-center p-1 ${customClass}`}>
-      <div className="grid rounded-md bg-orange-200 text-gray-800 px-3 h-16 place-content-center break-normal mr-3 ">
-        {date ? date.split(" ")[0] : "Jan"}
-        <br />
-        {date ? date.split(" ")[1] : "01"}
+    <div
+      className={`flex flex-row items-center justify-between p-1 ${customClass}`}
+    >
+      <div className="flex flex-row">
+        <div className="grid rounded-md bg-orange-200 text-gray-800 px-3 h-16 place-content-center break-normal mr-3 ">
+          {date ? date.split(" ")[0] : "Jan"}
+          <br />
+          {date ? date.split(" ")[1] : "01"}
+        </div>
+        <div className="flex flex-col text-sm text-gray-500 items-center">
+          <Label.Root className="font-bold text-md">{title}</Label.Root>
+          <Label.Root>{dateTimeStamp}</Label.Root>
+        </div>
       </div>
-      <div className="flex flex-col text-sm text-gray-500">
-        <Label.Root className="font-bold text-md">{title}</Label.Root>
-        <Label.Root>{dateTimeStamp}</Label.Root>
-      </div>
-      <BsCalendar4 className="text-orange-700 ml-16 text-3xl" />
+      <BsCalendar4 className="flex text-orange-700 ml-16 text-3xl" />
     </div>
   );
 }
