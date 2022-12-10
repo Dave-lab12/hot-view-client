50a3edd90906cab7c32b3cd7311ea751c47691ae
Yagth <yabuman46@gmail.com>
2022-12-10 19:22:12 +0300

    refactor: Schedule list added custom class

--- src/components/NewsComponents/ScheduleList.tsx
+++ src/components/NewsComponents/ScheduleList.tsx
@@ -5,39 +5,38 @@ import { ScheduleType } from "src/types/Schedule";
 import Schedule from "../HomePage/Schedule";
 
 interface IProps {
-  schedules: ScheduleType;
+  schedules?: ScheduleType;
+  className?: string;
 }
 
-function ScheduleList() {
+function ScheduleList({ schedules, className }: IProps) {
   return (
-    <>
-      <div className="border rounded-md xl:mt-5 p-2 h-fit">
-        <Label.Root className="text-xl text-blue-black font-bold">
-          Upcoming streams
-        </Label.Root>
-        <Schedule
-          title="Trade war in 2022"
-          date="Jan 02"
-          dateTimeStamp="Jan 10, 2022, 11:30 AM"
-          customClass="mt-5"
-        />
-        <Schedule
-          title="Trade war in 2022"
-          date="Jan 02"
-          dateTimeStamp="Jan 10, 2022, 11:30 AM"
-        />
-        <Schedule
-          title="Trade war in 2022"
-          date="Jan 02"
-          dateTimeStamp="Jan 10, 2022, 11:30 AM"
-        />
-        <Schedule
-          title="Trade war in 2022"
-          date="Jan 02"
-          dateTimeStamp="Jan 10, 2022, 11:30 AM"
-        />
-      </div>
-    </>
+    <div className={`border rounded-md xl:mt-5 p-2 h-fit ${className}`}>
+      <Label.Root className="text-xl text-blue-black font-bold">
+        Upcoming streams
+      </Label.Root>
+      <Schedule
+        title="Trade war in 2022"
+        date="Jan 02"
+        dateTimeStamp="Jan 10, 2022, 11:30 AM"
+        customClass="mt-5"
+      />
+      <Schedule
+        title="Trade war in 2022"
+        date="Jan 02"
+        dateTimeStamp="Jan 10, 2022, 11:30 AM"
+      />
+      <Schedule
+        title="Trade war in 2022"
+        date="Jan 02"
+        dateTimeStamp="Jan 10, 2022, 11:30 AM"
+      />
+      <Schedule
+        title="Trade war in 2022"
+        date="Jan 02"
+        dateTimeStamp="Jan 10, 2022, 11:30 AM"
+      />
+    </div>
   );
 }
 
