73f4218c65d6690a29071add56efd108ec615c66
Yagth <yabuman46@gmail.com>
2022-12-15 19:12:21 +0300

    feat: Article fetching endpoint

--- src/utils/articlesApi.tsx
+++ src/utils/articlesApi.tsx
@@ -1,19 +1,17 @@
 import axios from "axios";
 
-import { IArticle } from "../types/Article";
-
 const BASE_URL = "http://localhosst:8001/";
 
 export const articleApi = axios.create({
   baseURL: BASE_URL,
 });
 
-export const articles = async () => {
+export const getArticles = async () => {
   const response = await articleApi.get("/");
   return response;
 };
 
-export const article = async (id: string) => {
+export const getArticle = async (id: string) => {
   const response = await articleApi.get(`/:${id}`);
   return response;
 };
