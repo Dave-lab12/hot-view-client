14a60cad771794aa63041f6c44d88065e83b6065
Yagth <yabuman46@gmail.com>
2022-12-24 16:13:57 +0300

    feat: Hooks for fetching articles

--- src/hooks/useArticles.tsx
+++ src/hooks/useArticles.tsx
@@ -1,43 +1,17 @@
-import { useQuery, useMutation, UseQueryOptions } from "react-query";
+import { useQuery } from "react-query";
 
-import {
-  getArticles,
-  getArticle,
-  createArticle,
-  updateArticle,
-} from "../utils/articlesApi";
-import { IArticle } from "../types/Article";
+import { getArticles, getArticle } from "../utils/articlesApi";
 
 interface IParams {
   id: string;
 }
 
-const queryOptions = {
-  staleTime: 1000,
-  refetchOnMount: true,
-  refetchIntervalInBackground: true,
-};
-
-export function useGetArticles() {
-  const query = useQuery("Fetch Articles", getArticles, queryOptions);
+export function useArticles() {
+  const query = useQuery("Fetch Articles", getArticles);
   return query;
 }
 
-export function useGetArticle({ id }: IParams) {
-  const query = useQuery(
-    `Fetch Article ${id}`,
-    () => getArticle(id),
-    queryOptions
-  );
+export function useArticle({ id }: IParams) {
+  const query = useQuery("Fetch Articles", () => getArticle(id));
   return query;
-}
-
-export function useCreateArticle() {
-  const mutation = useMutation((article: IArticle) => createArticle(article));
-  return mutation;
-}
-
-export function useUpdateArticle() {
-  const mutation = useMutation((article: IArticle) => updateArticle(article));
-  return mutation;
 }
